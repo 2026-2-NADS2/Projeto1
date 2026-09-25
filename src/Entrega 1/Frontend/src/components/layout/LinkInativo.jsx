7390689ca71d5ef2d404971822bd5979a84e5bba
Dilly47 <dilly.martins@edu.fecap.br>
2026-09-25 20:47:23 -0300

    tentativa de integração com a main

--- src/Entrega 1/Frontend/src/components/layout/LinkInativo.jsx
+++
@@ -1,7 +0,0 @@
-export default function LinkInativo({ className, children }) {
-  return (
-    <a href="#" className={className} onClick={(e) => e.preventDefault()}>
-      {children}
-    </a>
-  );
-}
--- src/Entrega 1/Frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  /* GitHub Pages: base: '/nome-do-repositorio/' */
-  base: '/'
-});
